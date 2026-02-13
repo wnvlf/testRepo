241d31e6acad2c5b0fe71852b0183d2fc427d9e7
wnvlf <139468663+wnvlf@users.noreply.github.com>
2026-02-13 20:08:17 +0900

    feat: test2

--- Assets/test1.cs
+++ Assets/test1.cs
@@ -5,7 +5,7 @@ public class test1 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("test2");
     }
 
     // Update is called once per frame
